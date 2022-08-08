5935b676ee3b921af174a3a678bd6106ec82d5ad
danielhamam <danielhamam@outlook.com>
2022-08-08 16:56:11 -0400

    Configuration of font awesome and on-hover of portfolio projects styling

--- src/components/Portfolio/Project.js
+++ src/components/Portfolio/Project.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Project.css'
 
+// Font Awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+
 export const Project = (props) => {
   return (
-    <div>
+    <div className='proj-item-wrapper'>
         <img className="proj-image" src={props.image} alt="Project Image" />
+        <div className='proj-selection'></div>
+        <FontAwesomeIcon className='proj-icon1' icon={solid('eye')} />
+        <FontAwesomeIcon className='proj-icon2' icon={solid('arrow-up-right-from-square')} />
     </div>
   )
 }
