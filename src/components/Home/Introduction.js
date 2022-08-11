5a879e6f20d6d15993be7f7ac9a31370c8fc1d2b
danielhamam <danielhamam@outlook.com>
2022-08-11 12:46:08 -0400

    Fix Netlify Iframe Issue v2

--- src/components/Home/Introduction.js
+++ src/components/Home/Introduction.js
@@ -27,7 +27,7 @@ export default function Introduction() {
                 </div>
             </div>
             <div className='col-4'>
-                <iframe title="Coding Gif" id="intro-image" src="https://giphy.com/embed/ndIq5ohg1pyfqyVOII" frameBorder="0" class="giphy-embed"></iframe>
+                <a> <iframe title="Coding Gif" id="intro-image" src="https://giphy.com/embed/ndIq5ohg1pyfqyVOII" frameBorder="0" class="giphy-embed"></iframe> </a>
             </div>
         </div>
     </div>
