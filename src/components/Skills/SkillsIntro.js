550d7d9a6246d3fe0a1296db0e95e34cee2d327c
danielhamam <danielhamam@outlook.com>
2022-08-20 19:55:20 -0400

    Skills Intro Header Aligned Center on Mobile

--- src/components/Skills/SkillsIntro.js
+++ src/components/Skills/SkillsIntro.js
@@ -11,18 +11,18 @@ export default function SkillsIntro() {
         window.addEventListener("resize", handleResize)
     });
   return (
-    <div id="skills-wrapper" className="container-fluid d-flex justify-content-center align-items-center">
+    <div id="skills-wrapper" className="container d-flex justify-content-center align-items-center">
         <div className="row w-100">
-            <div className='col-6 d-flex justify-content-end align-items-center'>
+            <div className='col-7 col-xxl-6 d-flex justify-content-end align-items-center'>
                 <div className='mt-5 text-center'> 
                     <span id='skills-desc' className=''> 
-                    I program in all different <br/> kinds of ways. Catch me on all <br/> sides of the stack! 
+                        I program in all different <br/> kinds of ways. Catch me on all <br/> sides of the stack! 
                     </span>
                     <span className='custom-br-position'></span>
                     <button id='skills-button'> SCROLL DOWN </button>
                 </div>
             </div>
-            <div className='col-6 ps-5'>
+            <div className='col-5 col-xxl-6 ps-5 d-flex justify-content-center align-items-center"'>
                 <span className="wordsphere"></span>
             </div>
         </div>
