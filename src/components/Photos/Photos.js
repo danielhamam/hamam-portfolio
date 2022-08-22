3ec872e93207e17ff8537140d51f78effc9d585b
danielhamam <danielhamam@outlook.com>
2022-08-22 00:32:48 -0400

    Clear Blockers For Netlify Deployment

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -2,26 +2,6 @@ import React, { useEffect, useState } from 'react'
 import Smile from '../../assets/photo-album/Smile.png';
 import './Photos.css';
 
-// Importing photos
-
-// 4
-import baby from '../../assets/photo-album/photos/baby.jpg';
-import baba from '../../assets/photo-album/photos/baba.jpg';
-import beach from '../../assets/photo-album/photos/beach.jpg';
-import blackhat from '../../assets/photo-album/photos/blackhat.jpg';
-
-import brooklyn from '../../assets/photo-album/photos/brooklyn.jpg';
-import chelsea from '../../assets/photo-album/photos/chelsea.png';
-import city from '../../assets/photo-album/photos/city.jpg';
-import cousin from '../../assets/photo-album/photos/cousin.jpg';
-
-import friends from '../../assets/photo-album/photos/friends.png';
-import knicks from '../../assets/photo-album/photos/knicks.jpg';
-import mama from '../../assets/photo-album/photos/mama.jpg';
-import nyc from '../../assets/photo-album/photos/nyc.jpg';
-
-import timhortons from '../../assets/photo-album/photos/timhortons.jpg';
-
 export const Photos = () => {
     const [photos, setPhotos] = useState(null)
 
@@ -33,7 +13,7 @@ export const Photos = () => {
     useEffect(() => {
         // Receive photos and set it as state so it renders
         if (!photos) setPhotos(importPhotoAlbum(require.context('../../assets/photo-album/photos', false, /\.(png|jpe?g|svg)$/)));
-    })
+    }, [photos])
 
     return (
         <div id='photos-container' className='container'>
@@ -47,7 +27,7 @@ export const Photos = () => {
                     photos != null ? (photos.map((image, index) => {
                         return (
                             <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
-                                <img className='photo-item' key={index} src={image} alt="photo"/>
+                                <img className='photo-item' key={index} src={image} alt="life-snap"/>
                             </div>
                         )})) : 'Loading...'
                 }
