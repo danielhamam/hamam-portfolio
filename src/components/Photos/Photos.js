e7aa4f2258fb3be3d78dec5a1f4f91a1cc3d9872
danielhamam <danielhamam@outlook.com>
2022-08-22 00:25:28 -0400

    New Photos for Photo Album / Optimized Code To Randomize and Dynamically Render

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Smile from '../../assets/photo-album/Smile.png';
 import './Photos.css';
 
@@ -23,53 +23,35 @@ import nyc from '../../assets/photo-album/photos/nyc.jpg';
 import timhortons from '../../assets/photo-album/photos/timhortons.jpg';
 
 export const Photos = () => {
-  return (
-    <div id='photos-container' className='container'>
-        <div className='d-flex justify-content-center align-items-center py-5 my-5'>
-            <h1 id="photos-title" className='text-center'> Take a glance at a snapshot of my life </h1>
-            <img className="photos-smiley-icon mb-3 ms-3" src={Smile} alt="Smile"/>
-        </div>
-        <div className='row py-5'>
-            <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
-                <img className='photo-item' src={baby} alt="Baby"/>
+    const [photos, setPhotos] = useState(null)
+
+    function importPhotoAlbum(source) {
+        // Fetch photos, randomly sort them, place into map
+        return source.keys().sort(function(a,b) { return Math.random() - 0.5;}).map(source);
+    }
+
+    useEffect(() => {
+        // Receive photos and set it as state so it renders
+        if (!photos) setPhotos(importPhotoAlbum(require.context('../../assets/photo-album/photos', false, /\.(png|jpe?g|svg)$/)));
+    })
+
+    return (
+        <div id='photos-container' className='container'>
+            <div className='d-flex justify-content-center align-items-center py-5 my-5'>
+                <h1 id="photos-title" className='text-center'> Take a glance at a snapshot of my life </h1>
+                <img className="photos-smiley-icon mb-3 ms-3" src={Smile} alt="Smile"/>
             </div>
-            <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
-                <img className='photo-item' src={baba} alt="Baba"/>
-            </div>
-            <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
-                <img className='photo-item' src={beach} alt="Beach"/>
-            </div>
-            <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
-                <img className='photo-item' src={blackhat} alt="Black hat"/>
-            </div>
-            <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
-                <img className='photo-item' src={brooklyn} alt="Brooklyn"/>
-            </div>
-            <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
-                <img className='photo-item' src={chelsea} alt="Chelsea"/>
-            </div>
-            <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
-                <img className='photo-item' src={city} alt="City"/>
-            </div>
-            <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
-                <img className='photo-item' src={cousin} alt="Cousin"/>
-            </div>
-            <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
-                <img className='photo-item' src={friends} alt="Friends"/>
-            </div>
-            <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
-                <img className='photo-item' src={knicks} alt="Knicks"/>
-            </div>
-            <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
-                <img className='photo-item' src={mama} alt="Mama"/>
-            </div>
-            <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
-                <img className='photo-item' src={nyc} alt="NYC"/>
-            </div>
-            <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
-                <img className='photo-item' src={timhortons} alt="Tim Horton's"/>
+            <div className='row py-5'>
+                {/* Dynamically Loading Images From Photos State */}
+                {
+                    photos != null ? (photos.map((image, index) => {
+                        return (
+                            <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
+                                <img className='photo-item' key={index} src={image} alt="photo"/>
+                            </div>
+                        )})) : 'Loading...'
+                }
             </div>
         </div>
-    </div>
-  )
+    )
 }
