9516d0928fe13558e1560a6a8297b0376730e5e3
danielhamam <danielhamam@outlook.com>
2022-08-22 15:15:33 -0400

    Randomize / Dynamically Render Images While Scroll Down (Infinite Scroll) In Photo Album

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -3,7 +3,9 @@ import Smile from '../../assets/photo-album/Smile.png';
 import './Photos.css';
 
 export const Photos = () => {
-    const [photos, setPhotos] = useState(null)
+    const [photos, setPhotos] = useState(null);
+    const [numPhotos, setNumPhotos] = useState(12);
+    const [showLoadingPhotos, setShowLoadingPhotos] = useState(false)
 
     function importPhotoAlbum(source) {
         // Fetch photos, randomly sort them, place into map
@@ -12,8 +14,25 @@ export const Photos = () => {
 
     useEffect(() => {
         // Receive photos and set it as state so it renders
-        if (!photos) setPhotos(importPhotoAlbum(require.context('../../assets/photo-album/photos', false, /\.(png|jpe?g|svg)$/)));
-    }, [photos])
+        if (!photos) { setPhotos(importPhotoAlbum(require.context('../../assets/photo-album/photos', false, /\.(png|jpe?g|svg)$/))); }
+
+        // Check if user reached bottom of page
+        const loadImages = () => {
+            if ((window.innerHeight + Math.ceil(window.pageYOffset + 1)) >= document.body.offsetHeight) {
+                if (photos && numPhotos < Object.keys(photos).length) {
+                    let amountPhotosAfterLoad = numPhotos + 12 <= Object.keys(photos).length ? numPhotos + 12 : Object.keys(photos).length;
+                    // console.log('Loading more photos into album: ' + (amountPhotosAfterLoad) + '/' +  Object.keys(photos).length)
+                    setShowLoadingPhotos(true)
+                    setNumPhotos(amountPhotosAfterLoad);
+                    setShowLoadingPhotos(false)
+                }
+            }
+        }
+        window.addEventListener('scroll', loadImages);
+
+        // Imitating componentWillUnmount
+        return () => { window.removeEventListener('scroll', loadImages); }
+    }, [photos, numPhotos]) // Triggers the useEffect function to run when photos or numPhotos state changes
 
     return (
         <div id='photos-container' className='container'>
@@ -24,13 +43,17 @@ export const Photos = () => {
             <div className='row py-5'>
                 {/* Dynamically Loading Images From Photos State */}
                 {
-                    photos != null ? (photos.map((image, index) => {
+                    photos != null ? (photos.filter(function (image,index) { return index < numPhotos }).map((image, index) => {
                         return (
-                            <div className='col col-sm-6 col-lg-3 photo-item-wrapper'>
+                            <div className='col col-sm-6 col-lg-3 photo-item-wrapper' key={index} >
                                 <img className='photo-item' key={index} src={image} alt="life-snap"/>
                             </div>
-                        )})) : 'Loading...'
+                        )})) : 'Loading photos...'
                 }
+
+                {/* Check if More Photos are Loading */}
+                { showLoadingPhotos === true ? <div className='text-center mt-5' style={{fontWeight: 800}}> <span> Loading more photos...</span> </div> : ''}
+
             </div>
         </div>
     )
